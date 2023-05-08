be008da445781fb7d66011ecbb5a20618b827406
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-05-08 12:28:59 -0700

    Update OGL2D.h

--- src/OGL2D.h
+++ src/OGL2D.h
@@ -102,6 +102,7 @@ void OGL2D_Init(void);
 void OGL2D_SetWindowHint(int windowHint);
 void OGL2D_SetWindowColor(OGL2D_Color color);
 void OGL2D_CreateWindow(int windowWidth, int windowHeight, const char* windowTitle);
+void OGL2D_CloseWindow(void);
 void OGL2D_DestroyWindow(void);
 
 int OGL2D_WindowShouldClose(void);
