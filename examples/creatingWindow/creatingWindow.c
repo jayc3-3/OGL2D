9ab806b75ad43d2745b1bf559bce619e70c3e6eb
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-05-08 10:40:24 -0700

    Update creatingWindow.c

--- examples/creatingWindow/creatingWindow.c
+++ examples/creatingWindow/creatingWindow.c
@@ -7,7 +7,6 @@ int main(void){
 	OGL2D_SetWindowColor(OGL2D_GRAY); // Set window color to gray
 
 	while(!OGL2D_WindowShouldClose()){
-
 		// Without these, window will not appear
 		OGL2D_StartRendering();
 
